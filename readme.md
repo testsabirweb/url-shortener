e5c18b4bb0a12a2c60f96f6ed7358542cf99b620
Sabir <testsabirweb@gmail.com>
2024-09-03 16:43:25 +0530

    Added tests for top domains

---
+++ readme.md
@@ -0,0 +1,1 @@
+docker hub url: https://hub.docker.com/repository/docker/sabir9644/url-shortener/general
--- store/store_service_test.go
+++ store/store_service_test.go
@@ -22,6 +22,49 @@ func TestInsertionAndRetrieval(t *testing.T) {
 	shortURL := "something"
 
 	SaveUrlMapping(shortURL, initialLink)
-	originalUrl, _ := RetrieveInitialUrl(shortURL)
-	assert.Equal(t, initialLink, originalUrl)
+	originalUrl, err := RetrieveInitialUrl(shortURL)
+
+	assert.NoError(t, err, "Error should be nil")
+	assert.Equal(t, initialLink, originalUrl, "Retrieved URL should match the initial link")
+}
+
+func TestExtractDomain(t *testing.T) {
+	tests := []struct {
+		originalUrl string
+		expected    string
+	}{
+		{"https://www.infracloud.io", "infracloud.io"},
+		{"https://infracloud.io", "infracloud.io"},
+		{"http://www.example.com", "example.com"},
+		{"http://example.com", "example.com"},
+		{"ftp://www.ftpserver.com", "ftpserver.com"},
+		{"ftp://ftpserver.com", "ftpserver.com"},
+		{"invalid-url", ""},                // Invalid URL
+		{"https://sub.domain.com", "sub.domain.com"}, // Subdomain
+	}
+
+	for _, test := range tests {
+		domain := extractDomain(test.originalUrl)
+		assert.Equal(t, test.expected, domain)
+	}
+}
+
+func TestGetTopDomains(t *testing.T) {
+	// Reset domainCount for a clean test environment
+	storeService.domainCount = map[string]int{
+		"infracloud.io":  5,
+		"example.com":    10,
+		"test.com":       3,
+		"another.com":    7,
+	}
+
+	expectedTopDomains := map[string]int{
+		"example.com":  10,
+		"another.com":  7,
+		"infracloud.io": 5,
+	}
+
+	topDomains := GetTopDomains()
+
+	assert.Equal(t, expectedTopDomains, topDomains)
 }
