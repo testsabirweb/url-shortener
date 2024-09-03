7b44944f21bf734bbcedc5c34e0fe98a1b6e02f0
Sabir <testsabirweb@gmail.com>
2024-09-03 11:15:54 +0530

    shorturl function generated

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.2
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/itchyny/base58-go v0.2.2
 	github.com/stretchr/testify v1.9.0
 )
 
---
+++ shortener/shorturl_generator_test.go
@@ -0,0 +1,22 @@
+package shortener
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+
+func TestShortLinkGenerator(t *testing.T) {
+	initialLink_1 := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
+	shortLink_1 := GenerateShortLink(initialLink_1)
+
+	initialLink_2 := "https://www.eddywm.com/lets-build-a-url-shortener-in-go-with-redis-part-2-storage-layer/"
+	shortLink_2 := GenerateShortLink(initialLink_2)
+
+	initialLink_3 := "https://spectrum.ieee.org/automaton/robotics/home-robots/hello-robots-stretch-mobile-manipulator"
+	shortLink_3 := GenerateShortLink(initialLink_3)
+
+	assert.Equal(t, shortLink_1, "ZV7FREWM")
+	assert.Equal(t, shortLink_2, "hFYku75v")
+	assert.Equal(t, shortLink_3, "AFZXB5Hs")
+}
